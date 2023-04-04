2004866c45a1a4a88694e6fc7fa2012eea1c8690
Ruslan <saharcorez@gmail.com>
2023-04-04 18:33:48 +0300

    Исправления в меню

--- js/header-menu.js
+++ js/header-menu.js
@@ -5,12 +5,14 @@ window.addEventListener("DOMContentLoaded", function () {
   const headerMenu = document.querySelector(".header-menu");
   const mobileMenuButtonService = document.querySelector(".mobile-menu__button-service");
   const swipeMenu = document.querySelector(".swipe-menu");
-  const swipeMenuContainer = swipeMenu.querySelector(".swipe-menu__container")
+  const swipeMenuContainer = swipeMenu.querySelector(".swipe-menu__container");
+  const menuBackground = document.querySelector(".menu-background");
   //const swipeMenuItem = document.querySelector(".swipe-menu__scroll-item");
 
   function handleMobileMenu() {
     mobileMenuButtonService.classList.toggle("mobile-menu__button-service_active");
     swipeMenu.classList.toggle("swipe-menu_active");
+    menuBackground.classList.toggle("menu-background_active");
 
     if (html.style.overflow === "hidden") {
       html.style.overflow = "visible";
@@ -25,12 +27,14 @@ window.addEventListener("DOMContentLoaded", function () {
     html.style.overflow = "visible";
     mobileMenuButtonService.classList.remove("mobile-menu__button-service_active");
     swipeMenu.classList.remove("swipe-menu_active");
+    menuBackground.classList.remove("menu-background_active");
     swipeMenu.style.transform = `translateY(100%)`;
   }
 
   headerButtonService.addEventListener("click", () => {
     headerWithMenu.classList.toggle("header-menu_opened");
     headerMenu.classList.toggle("header-menu_open");
+    menuBackground.classList.toggle("menu-background_active");
 
     if (html.style.overflow === "hidden") {
       html.style.overflow = "visible";
@@ -57,7 +61,7 @@ window.addEventListener("DOMContentLoaded", function () {
   swipeMenu.addEventListener('touchend', function (event) {
     touchendY = event.changedTouches[0].screenY;
 
-    if ((movedY - touchstartY > 30) && (swipeMenuContainer.scrollTop === 0 || !event.target.closest(".swipe-menu__container"))) {
+    if ((movedY - touchstartY > 30) && (swipeMenuContainer.scrollTop <= 0 || !event.target.closest(".swipe-menu__container"))) {
       mobileMenuClose();
     } else {
       swipeMenu.style.transform = `translateY(0)`;
