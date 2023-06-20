06b4fa6e59c365354b2dd36f02be44ce9e528feb
Ruslan <saharcorez@gmail.com>
2023-06-20 15:08:51 +0300

    Табы

---
+++ js/combined-tabs.js
@@ -0,0 +1,47 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const tabsContainer = document.querySelector(".combined-tabs__list");
+  const tabsBtns = tabsContainer.querySelectorAll(".tabs__item");
+  const tabsItems = tabsContainer.querySelectorAll(".serves__options");
+
+  function onTabClick(item) {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = tabsContainer.querySelector(tabId);
+
+    if (!currentBtn.classList.contains('tabs__item-active')) {
+      tabsBtns.forEach(function (item) {
+        item.classList.remove('tabs__item-active');
+      });
+
+      tabsItems.forEach(function (item) {
+        item.classList.remove('serves__options-active');
+      });
+
+      currentBtn.classList.add('tabs__item-active');
+      currentTab.classList.add('serves__options-active');
+    }
+  }
+
+  tabsItems.forEach((item) => {
+    if (item.querySelector(".combined-tabs__head")) {
+      const tabItem = tabsContainer.querySelector(`[data-tab="#${item.id}"]`);
+
+      item.querySelector(".combined-tabs__head").addEventListener("click", () => {
+        tabsBtns.forEach(function (item) {
+          item.classList.remove('tabs__item-active');
+        });
+
+        tabsItems.forEach(function (tab) {
+          tab.classList.remove("serves__options-active");
+        });
+
+        tabItem.classList.add("tabs__item-active");
+        item.classList.add("serves__options-active");
+      })
+    }
+  })
+
+  tabsBtns.forEach((item) => {
+    item.addEventListener("click", () => onTabClick(item));
+  })
+})
