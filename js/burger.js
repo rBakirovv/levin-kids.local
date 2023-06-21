914f485605d6db68cd01d8655caf430eb5640106
Ruslan <saharcorez@gmail.com>
2023-06-21 20:56:03 +0300

    Бургер

--- js/burger.js
+++ js/burger.js
@@ -1,13 +1,16 @@
 window.addEventListener("DOMContentLoaded", function () {
+  const html = document.querySelector("html");
   const menu = document.querySelector(".burger-menu-wrap");
   const menuCloseBtn = document.querySelector(".burger-menu-header__close");
   const menuOpenBtn = document.querySelector(".header__burger");
 
   const openMenu = () => {
+    html.style.overflow = "hidden";
     menu.classList.add("burger-menu-wrap--active");
   };
   const closeMenu = () => {
     menu.classList.remove("burger-menu-wrap--active");
+    html.style.overflow = "visible";
   };
 
   menu.addEventListener("click", (e) => {
